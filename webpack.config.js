3deef70698b5d2d4188b5ef65aad8db448e422c9
Thomas DeCelle <tdecelle@gmail.com>
2018-06-28 18:10:19 -0600

    Added js folder to public
    updated the webpack and .gitignore for the changed bundle.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 const config = {
   entry: './src/client/client_game.js',
   output: {
-    path: path.resolve(__dirname, 'public'),
+    path: path.resolve(__dirname, 'public/js'),
     filename: 'bundle.js'
   }
 };

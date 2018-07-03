ef0a01cd2b023088ef4a398fe8f02b3aea684f36
Thomas DeCelle <tdecelle@gmail.com>
2018-07-02 19:40:53 -0600

    Added Full Screen
    won't adjust when window is resized

--- src/client/rendering/changeCanvasToFull.js
+++ src/client/rendering/changeCanvasToFull.js
@@ -1,5 +1,4 @@
 const changeCanvasToFull = module.exports = function() {
-  console.log("Change Canvas to FULL");
   let canvas = document.getElementById("canvas");
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
